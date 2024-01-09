d49070892715414e65f6d535f02f3f0aee4d79d5
bayrem98 <bayremsayeh98@gmail.com>
2024-01-09 17:34:08 +0100

    update code for fix error upload file payload large size video

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -8,8 +8,8 @@ async function bootstrap() {
       origin: '*',
     },
   });
-  app.use(bodyParser.json({ limit: '100mb' })); // ajustez la limite selon vos besoins
-  app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+  app.use(json({ limit: '100mb' })); // pour le corps de la demande JSON
+  app.use(urlencoded({ limit: '100mb', extended: true })); // pour le corps de la demande codé en URL
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
