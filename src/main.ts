3c17fae78b3ac425747a8a2991d118736a461348
bayrem98 <bayremsayeh98@gmail.com>
2024-01-09 12:09:22 +0100

    update code in main.ts for cors app

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'https://voyance-global.vercel.app',
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      credentials: true,
+      origin: '*',
     },
   });
-  app.use(bodyParser.json({ limit: '50mb' })); // Augmentez la limite selon vos besoins
-  app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
