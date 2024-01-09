cb6612393a8f539e52f0d6421d4104885b12dde2
bayrem98 <bayremsayeh98@gmail.com>
2024-01-09 16:59:07 +0100

    add new dependency to fix error payload large size file upload

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -7,6 +8,8 @@ async function bootstrap() {
       origin: '*',
     },
   });
+  app.use(bodyParser.json({ limit: '50mb' })); // ajustez la limite selon vos besoins
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
