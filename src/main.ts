ffe1c84523ca0b54f9782256f5b42506bf97c617
bayrem98 <bayremsayeh98@gmail.com>
2024-01-09 10:32:19 +0100

    update code in main.ts to fix error cors & large size video upload

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: 'https://voyance-global.vercel.app',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
     },
   });
+  app.use(bodyParser.json({ limit: '50mb' })); // Augmentez la limite selon vos besoins
+  app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
