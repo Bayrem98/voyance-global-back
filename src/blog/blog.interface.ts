15e39e1d380d10d36630db16a537bf11023005d7
bayrem98 <bayremsayeh98@gmail.com>
2024-03-22 13:10:17 +0100

    Ajout de .env à .gitignore

--- src/blog/blog.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface Blog {
-  coverPath: string;
-  videoPath: string;
-  posterPath: string;
-}
--- src/blog/dto/create-blog.dto.ts
+++
@@ -1,5 +0,0 @@
-export default class CreateBlogDto {
-  coverPath: string;
-  videoPath: string;
-  posterPath: string;
-}
